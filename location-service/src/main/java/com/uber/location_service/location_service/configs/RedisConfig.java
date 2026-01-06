5cca0547977bdc533ec8128b594b11ca53e53968
DkDeepak001 <deswar@grepruby.io>
2026-01-06 19:17:36 +0530

    config: docker-compose

--- location-service/src/main/java/com/uber/location_service/location_service/configs/RedisConfig.java
+++ location-service/src/main/java/com/uber/location_service/location_service/configs/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.uber.location_service.location_service.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -9,9 +10,18 @@ import org.springframework.data.redis.core.RedisTemplate;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
-        return new JedisConnectionFactory();
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+        jedisConnectionFactory.getStandaloneConfiguration().setHostName(redisHost);
+        jedisConnectionFactory.getStandaloneConfiguration().setPort(redisPort);
+        return jedisConnectionFactory;
     }
 
     @Bean
